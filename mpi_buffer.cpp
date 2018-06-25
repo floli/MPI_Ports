8f616e861eeab54265ba02e1c0a8db755f15955e
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-06-25 10:16:44 +0200

    Add Isend test method.

--- mpi_buffer.cpp
+++ mpi_buffer.cpp
@@ -17,6 +17,23 @@ void send()
   }
 }
 
+/// Tests the amount of data that can be send, without a matching receive.
+void Isend()
+{
+  double sizeofint = sizeof(int) / 1024.0;
+  INFO << "sizeof(int) = " << sizeof(int);
+  int i = 0;
+  while (true) {
+    INFO << "Iteration " << i << " data send = " << i * sizeofint << " KB";
+    INFO << sizeofint;
+    MPI_Request req;
+    MPI_Isend(&i, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &req);
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+    ++i;
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   MPI_Init(&argc, &argv);
