a6f2ae48d4e92b1a0542da63e0fdf2ad26e162de
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-06-26 11:04:31 +0200

    Working version of buffer mananger.

---
+++ buffer_manager.hpp
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <mpi.h>
+#include <memory>
+#include <deque>
+#include <mutex>
+#include <boost/variant.hpp>
+class Request;
+using PtrRequest = std::shared_ptr<Request>;
+
+
+class Request
+{
+public:
+  MPI_Request _request;
+  
+  Request(MPI_Request request)
+    : _request(request)
+  {}
+
+  void wait(std::vector<PtrRequest> &requests)
+  {
+    for (auto request : requests) {
+      request->wait();
+    }
+  }
+
+  bool test()
+  {
+    int complete = 0;
+    MPI_Test(&_request, &complete, MPI_STATUS_IGNORE);
+    return complete;
+  }
+
+  void wait()
+  {
+    MPI_Wait(&_request, MPI_STATUS_IGNORE);
+  }
+};
+
+class release_visitor : public boost::static_visitor<>
+{
+public:
+  template<typename T>
+  void operator()(T & operand) const
+  {
+    operand.release();
+  }
+};
+
+class is_valid_ptr_visitor : public boost::static_visitor<bool>
+{
+public:
+  template<typename T>
+  bool operator()(T & operand) const
+  {
+    return static_cast<bool>(operand);
+  }
+};
+
+
+class BufferManager
+{
+public:
+  /// Deleted copy operator for singleton pattern
+  BufferManager(BufferManager const &) = delete;
+  
+  /// Deleted assigment operator for singleton pattern
+  void operator=(BufferManager const &) = delete;
+
+  static BufferManager & instance()
+  {
+    static BufferManager instance;
+    return instance;
+  }
+
+  template<typename T>
+  void put(Request request, std::unique_ptr<T> buffer)
+  {
+    std::lock_guard<std::mutex> lock(bufferedRequestsMutex);
+    // auto p = std::make_pair(request, std::move(buffer));
+    bufferedRequests.emplace_back(request, std::move(buffer));
+  }
+
+  void run()
+  {
+    INFO << "Starting thread";
+    thread = std::thread(&BufferManager::check, this);
+  }
+
+  void stop()
+  {
+    stopFlag = true;
+    thread.join();
+  }
+
+  
+  void check()
+  {
+    INFO << "Check thread started";
+    while (not stopFlag) {
+      for (auto it = bufferedRequests.begin(); it != bufferedRequests.end();) {
+        if (boost::apply_visitor(is_valid_ptr_visitor(), it->second) and it->first.test()) {
+          std::lock_guard<std::mutex> lock(bufferedRequestsMutex);
+          // it->second.release();
+          boost::apply_visitor(release_visitor(), it->second);
+          DEBUG << "Released a ptr, #requests = " << bufferedRequests.size();
+          it = bufferedRequests.erase(it);
+          sleep(100);
+        }
+        else {
+          ++it;
+        }
+      }
+      std::this_thread::yield();
+    }
+  }
+    
+
+private:
+  /// Private, empty constructor for singleton pattern
+  BufferManager() {}
+
+  using ptr_types = boost::variant< std::unique_ptr<int>, std::unique_ptr<double> >;
+
+  // std::deque<std::pair<Request, std::unique_ptr<int>>> bufferedRequests;
+  std::deque<std::pair<Request, ptr_types>> bufferedRequests;
+  
+
+  std::thread thread;
+  std::mutex bufferedRequestsMutex;
+  bool stopFlag = false;
+};
---
+++ ports_send_managed.cpp
@@ -0,0 +1,101 @@
+#include <mpi.h>
+#include <memory>
+#include "common.hpp"
+#include "buffer_manager.hpp"
+
+MPI_Comm accept()
+{
+  MPI_Comm icomm;
+  std::string port = openPort();
+  writePort("publish" / boost::filesystem::path("ports_send_buffer.address"), port);
+  MPI_Comm_accept(port.c_str(), MPI_INFO_NULL, 0, MPI_COMM_WORLD, &icomm);
+  INFO << "Sucessfully accepted connection.";
+  return icomm;
+}
+
+MPI_Comm request()
+{
+  MPI_Comm icomm;
+  std::string port = readPort("publish" / boost::filesystem::path("ports_send_buffer.address"));
+  MPI_Comm_connect(port.c_str(), MPI_INFO_NULL, 0, MPI_COMM_WORLD, &icomm);
+  INFO << "Sucessfully connected.";
+  return icomm;
+}
+
+/// Tests the amount of data that can be send, without a matching receive.
+void Send(MPI_Comm comm)
+{
+  double sizeofint = sizeof(int) / 1024.0;
+  INFO << "sizeof(int) = " << sizeof(int);
+  int i = 0;
+  while (true) {
+    INFO << "Iteration " << i << ", data send = " << i * sizeofint << " KB";
+    MPI_Send(&i, 1, MPI_INT, 0, 0, comm);
+    ++i;
+  }
+}
+
+/// Tests the amount of data that can be send, without a matching receive.
+void Isend(MPI_Comm comm)
+{
+  double sizeofint = sizeof(int) / 1024.0;
+  INFO << "sizeof(int) = " << sizeof(int);
+  int i = 0;
+  while (true) {
+    INFO << "Iteration " << i << ", data send = " << i * sizeofint << " KB";
+    MPI_Request req;
+    auto sb = std::unique_ptr<double>(new double);
+    *sb = i;
+    MPI_Isend(sb.get(), 1, MPI_DOUBLE, 0, 0, comm, &req);
+    BufferManager::instance().put(Request(req), std::move(sb));
+    // MPI_Wait(&req, MPI_STATUS_IGNORE);
+    // sleep(100);
+    ++i;
+  }
+}
+
+/// Delayed receive
+void Receive(MPI_Comm comm)
+{
+  double sizeofint = sizeof(double) / 1024.0;
+  INFO << "sizeof(int) = " << sizeof(double);
+  double i = 0;
+  while (true) {
+    MPI_Recv(&i, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, MPI_STATUS_IGNORE);
+    INFO << "Received " << i << ", data received = " << i * sizeofint << " KB";
+    sleep(1000);
+  }
+}
+
+
+/// Launch two mpirun -n 1 processes
+int main(int argc, char **argv)
+{
+  // MPI_Init(&argc, &argv);
+  int required = MPI_THREAD_MULTIPLE;
+  int provided;
+  MPI_Init_thread(&argc, &argv, required, &provided);
+  assert(required == provided);
+  logging::init();
+
+  std::string participant = argv[1];
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);  
+
+  MPI_Comm icomm;
+  if (participant == "A") {
+    auto & bm = BufferManager::instance();
+    bm.run();
+    icomm = accept();
+    Isend(icomm);
+    // Send(icomm);    
+  }
+  if (participant == "B") {
+    icomm = request();
+    Receive(icomm);
+  }
+
+  
+  MPI_Finalize();
+}
