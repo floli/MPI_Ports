f8a043eeedd6eb7cc033a706f1251bf005b0d324
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-07-18 15:23:09 +0200

    Add tests for invertGetRanks.

--- tests.cpp
+++ tests.cpp
@@ -32,3 +32,23 @@ BOOST_AUTO_TEST_CASE(testGetRanksRatio)
   BOOST_TEST( getRanks(0.5, 10, 3) == std::vector<int>({1, 2, 3, 4, 5}) );
   BOOST_TEST( getRanks(0.8, 10, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
 }
+
+BOOST_AUTO_TEST_CASE(testInvertGetRanksAbsolute)
+{
+  BOOST_TEST( invertGetRanks(2, 8, 3) == std::vector<int>({3, 4}) );
+  BOOST_TEST( invertGetRanks(3, 8, 3) == std::vector<int>({2, 3, 4}) );
+  BOOST_TEST( invertGetRanks(4, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5}) );
+  BOOST_TEST( invertGetRanks(5, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+  BOOST_TEST( invertGetRanks(6, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+  BOOST_TEST( invertGetRanks(7, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+  BOOST_TEST( invertGetRanks(8, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+
+  BOOST_TEST( invertGetRanks(8, 80, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+  
+  BOOST_TEST( invertGetRanks(3, 80, 0) == std::vector<int>({0, 1}) );
+
+  BOOST_TEST( invertGetRanks(4, 5, 4) == std::vector<int>({3, 4}) );
+
+  BOOST_TEST( invertGetRanks(4, 5, 4) == std::vector<int>({3, 4}) );
+ 
+}
