f84700816ec9b43171d0c749932f474c8339686d
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-07-18 15:11:11 +0200

    Add tests.

---
+++ tests.cpp
@@ -0,0 +1,34 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "common.hpp"
+#include "prettyprint.hpp"
+#include <iostream>
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(testGetRanksAbsolute)
+{
+  BOOST_TEST( getRanks(2, 8, 3) == std::vector<int>({2, 3}) );
+  BOOST_TEST( getRanks(3, 8, 3) == std::vector<int>({2, 3, 4}) );
+  BOOST_TEST( getRanks(4, 8, 3) == std::vector<int>({1, 2, 3, 4}) );
+  BOOST_TEST( getRanks(5, 8, 3) == std::vector<int>({1, 2, 3, 4, 5}) );
+  BOOST_TEST( getRanks(6, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5}) );
+  BOOST_TEST( getRanks(7, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6}) );
+  BOOST_TEST( getRanks(8, 8, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+
+  BOOST_TEST( getRanks(8, 80, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+  
+  BOOST_TEST( getRanks(3, 80, 0) == std::vector<int>({0, 1, 2}) );
+
+  BOOST_TEST( getRanks(4, 5, 4) == std::vector<int>({1, 2, 3, 4}) );
+
+  BOOST_TEST( getRanks(4, 5, 4) == std::vector<int>({1, 2, 3, 4}) );
+ 
+}
+
+BOOST_AUTO_TEST_CASE(testGetRanksRatio)
+{
+  BOOST_TEST( getRanks(0.2, 10, 3) == std::vector<int>({2, 3}) );
+  BOOST_TEST( getRanks(0.5, 10, 3) == std::vector<int>({1, 2, 3, 4, 5}) );
+  BOOST_TEST( getRanks(0.8, 10, 3) == std::vector<int>({0, 1, 2, 3, 4, 5, 6, 7}) );
+}
